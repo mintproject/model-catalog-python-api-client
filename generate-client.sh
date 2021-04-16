d5a71cd6330fe5af6b2802ee30bf57ae8cbaed94
Maximiliano Osorio <maxiosorio@gmail.com>
2021-04-16 08:59:43 -0400

    fix: generate-client

--- generate-client.sh
+++ generate-client.sh
@@ -13,8 +13,6 @@ docker run -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.1.2 \
      --git-repo-id model-catalog-python-api-client \
      --git-user-id mintproject  \
 
-mv -f docs/ModelconfigurationApi.md docs/ModelConfigurationApi.md
-
 sed -i 's~docs/~docs/endpoints/~g' README.md
 mv docs/[A-Z]*.md docs/endpoints/
 pushd docs/endpoints/
